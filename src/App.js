27d9e03bd1e5659d5c3c1e0bf84537d3b98481c3
DinukiFernando <86251449+DinukiFernando@users.noreply.github.com>
2025-01-02 19:32:34 +0530

    bug fix commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contacts';
+
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        {/* <nav className="navbar">
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/contact">Contact</Link>
-        </nav>
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </div> */}
         <Home/>
       </div>
     </Router>
